4d90e8bec0fce974018cddd867ca5beda1a89a76
sabithabandi <Sabitha.Dachepalli@wellsfargo.com>
2017-05-10 08:42:46 -0700

    Added DYnamic port

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -2,15 +2,21 @@ package com.mulesoft.training;
 
 import static org.junit.Assert.*;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.mule.api.MuleEvent;
 import org.mule.tck.junit4.FunctionalTestCase;
+import org.mule.tck.junit4.rule.DynamicPort;
 
 public class HelloMavenTest extends FunctionalTestCase {
 
+	
+	@Rule
+	public DynamicPort dynamicPort = new DynamicPort("http.port");
     // mavenFlow returns Hello Maven
     @Test
     public void mavenFlowReturnsHelloMaven() throws Exception {
+    	System.out.println (" Dyn Port is"+dynamicPort.getNumber());
         runFlowAndExpect("mavenFlow", "Hello Maven");
     }
        
@@ -24,6 +30,7 @@ public class HelloMavenTest extends FunctionalTestCase {
     // content-type headers has a value of application/json
    @Test
     public void retrieveFlightsAddsAppropriateHeader() throws Exception{
+	   System.out.println (" Dyn Port is"+dynamicPort.getNumber());
     	MuleEvent event = runFlow("retrieveFlights");
     	String contentType = event.getMessage().getOutboundProperty("Content-Type");
     	assertEquals("application/json", contentType);
